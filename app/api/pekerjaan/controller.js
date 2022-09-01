12b25cf679668266042d7635d034335d34c26e84
FarBb <loveinsemarang@gmail.com>
2022-09-02 01:58:25 +0700

    Created Pekerjaan

--- app/api/pekerjaan/controller.js
+++ app/api/pekerjaan/controller.js
@@ -1,4 +1,7 @@
 const { Pekerjaan } = require('../../db/models');
+const path = require("path");
+const fs = require("fs");
+const config = require("../../../config");
 
 module.exports = {
   getPekerjaanFrontEnd: async (req, res, next) => {
@@ -28,6 +31,44 @@ module.exports = {
     try {
       const apiData = await Pekerjaan.findAll()
       res.status(200).json(apiData);
+    } catch (err) {
+      res.status(500).json({ message: err.message || `Internal server error` });
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { pekerjaan, deskripsiPekerjaan } = req.body;
+      if (req.file) {
+        let tmp_path = req.file.path;
+        let originaExt = req.file.originalname.split(".")[req.file.originalname.split(".").length - 1];
+        let filename = req.file.filename + "." + originaExt;
+        let target_path = path.resolve(config.rootPath, `public/uploads/banner-pekerjaan/${filename}`);
+
+        const src = fs.createReadStream(tmp_path);
+        const dest = fs.createWriteStream(target_path);
+
+        src.pipe(dest);
+        src.on("end", async () => {
+          try {
+            const apiData = new Pekerjaan({
+              pekerjaan,
+              deskripsiPekerjaan,
+              image: filename,
+            });
+            await apiData.save();
+            res.status(201).json({ msg: "success", data: apiData });
+          } catch (err) {
+            res.status(500).json({ message: err.message || `Internal server error` });
+          }
+        });
+      } else {
+        const apiData = new Pekerjaan({
+          pekerjaan,
+          deskripsiPekerjaan,
+        });
+        await apiData.save();
+        res.status(201).json({ msg: "success", data: apiData });
+      }
     } catch (err) {
       res.status(500).json({ message: err.message || `Internal server error` });
     }
--- app/api/pekerjaan/router.js
+++ app/api/pekerjaan/router.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const multer = require("multer");
+const os = require("os");
 
-const { getPekerjaanFrontEnd, getDetailsPekerjaanFrontEnd, getPekerjaanBackend, } = require('./controller');
+const { getPekerjaanFrontEnd, getDetailsPekerjaanFrontEnd, getPekerjaanBackend, actionCreate, } = require('./controller');
 const { isLoginApiAdmin } = require('../../middleware/auth')
 
 router.get('/', getPekerjaanFrontEnd);
 router.get('/pekerjaanbackend', isLoginApiAdmin, getPekerjaanBackend);
+router.post("/add", isLoginApiAdmin, multer({ dest: os.tmpdir() }).single("image"), actionCreate);
 router.get('/:id', getDetailsPekerjaanFrontEnd);
 
 module.exports = router;
