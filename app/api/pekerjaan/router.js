cad4e7e6c59f69541c16b96d134d8697fecfec98
FarBb <loveinsemarang@gmail.com>
2022-09-02 01:45:38 +0700

    Middleware with JWT

--- app/api/pekerjaan/router.js
+++ app/api/pekerjaan/router.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 const { getPekerjaanFrontEnd, getDetailsPekerjaanFrontEnd, getPekerjaanBackend, } = require('./controller');
+const { isLoginApiAdmin } = require('../../middleware/auth')
 
 router.get('/', getPekerjaanFrontEnd);
-router.get('/pekerjaanbackend', getPekerjaanBackend);
+router.get('/pekerjaanbackend', isLoginApiAdmin, getPekerjaanBackend);
 router.get('/:id', getDetailsPekerjaanFrontEnd);
 
 module.exports = router;
