21dc003e689d0632e1d06949748a405477283e04
FarBb <loveinsemarang@gmail.com>
2022-09-02 02:03:47 +0700

    Upload Image Pekerjaan

--- app/api/pekerjaan/router.js
+++ app/api/pekerjaan/router.js
@@ -3,12 +3,13 @@ var router = express.Router();
 const multer = require("multer");
 const os = require("os");
 
-const { getPekerjaanFrontEnd, getDetailsPekerjaanFrontEnd, getPekerjaanBackend, actionCreate, } = require('./controller');
+const { getPekerjaanFrontEnd, getDetailsPekerjaanFrontEnd, getPekerjaanBackend, actionCreate, uploadFile, } = require('./controller');
 const { isLoginApiAdmin } = require('../../middleware/auth')
 
 router.get('/', getPekerjaanFrontEnd);
 router.get('/pekerjaanbackend', isLoginApiAdmin, getPekerjaanBackend);
 router.post("/add", isLoginApiAdmin, multer({ dest: os.tmpdir() }).single("image"), actionCreate);
+router.put("/up/:id", isLoginApiAdmin, multer({ dest: os.tmpdir() }).single("image"), uploadFile);
 router.get('/:id', getDetailsPekerjaanFrontEnd);
 
 module.exports = router;
